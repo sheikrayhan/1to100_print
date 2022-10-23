5f19a1b1aed5ad190e6156bd2314b4433bb7a51e
sheikrayhan <112091560+sheikrayhan@users.noreply.github.com>
2022-10-23 17:34:21 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+for (var i = 1; i <= 500; i++) {
+    if (i % 2 ==0) {
+        document.write(i + "=(This is Even Number)&#128525." + "<br> <br>");
+    }else{
+        document.write(i + "=(This is Odd Number)&#128523." + "<br> <br>");
+    }
+}
